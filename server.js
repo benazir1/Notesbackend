13d50be92379a67fe1cfa9fc28d90e1059b39779
benazir1 <benazir1989@gmail.com>
2023-11-18 15:36:40 +0530

    Changes done

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ mongoose.connect(URI,err=>{
 })
 
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) =>{
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    });
-}
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static('client/build'));
+//     app.get('*', (req, res) =>{
+//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+//     });
+// }
 
 
 //listen server
